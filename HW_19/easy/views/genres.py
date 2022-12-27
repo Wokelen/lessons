f05083391b7e6e9250a6df64996f09182786d2aa
Wokelen <99183991+Wokelen@users.noreply.github.com>
2022-12-27 16:31:01 +0300

    Add files via upload

---
+++ HW_19/easy/views/genres.py
@@ -0,0 +1,25 @@
+from flask_restx import Resource, Namespace
+
+from decorators import auth_required
+from models import Genre, GenreSchema
+from setup_db import db
+
+genre_ns = Namespace('genres')
+
+
+@genre_ns.route('/')
+class GenresView(Resource):
+    @auth_required
+    def get(self):
+        rs = db.session.query(Genre).all()
+        res = GenreSchema(many=True).dump(rs)
+        return res, 200
+
+
+@genre_ns.route('/<int:rid>')
+class GenreView(Resource):
+    @auth_required
+    def get(self, rid):
+        r = db.session.query(Genre).get(rid)
+        sm_d = GenreSchema().dump(r)
+        return sm_d, 200
