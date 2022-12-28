0b1c5623bcea5eab692bc12516e11538e0833207
Wokelen <99183991+Wokelen@users.noreply.github.com>
2022-12-29 01:10:28 +0300

    Add files via upload

---
+++ HW_19/easy/models.py
@@ -0,0 +1,108 @@
+import calendar
+import datetime
+import hashlib
+
+import jwt
+from marshmallow import Schema, fields
+
+from constants import JWT_SECRET, JWT_ALGORITHM
+from setup_db import db
+
+
+class Movie(db.Model):
+    __tablename__ = 'movie'
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(255))
+    description = db.Column(db.String(255))
+    trailer = db.Column(db.String(255))
+    year = db.Column(db.Integer)
+    rating = db.Column(db.Float)
+    genre_id = db.Column(db.Integer, db.ForeignKey("genre.id"))
+    genre = db.relationship("Genre")
+    director_id = db.Column(db.Integer, db.ForeignKey("director.id"))
+    director = db.relationship("Director")
+
+
+class MovieSchema(Schema):
+    id = fields.Int()
+    title = fields.Str()
+    description = fields.Str()
+    trailer = fields.Str()
+    year = fields.Int()
+    rating = fields.Float()
+
+
+class Director(db.Model):
+    __tablename__ = 'director'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(255))
+
+
+class DirectorSchema(Schema):
+    id = fields.Int()
+    name = fields.Str()
+
+
+class Genre(db.Model):
+    __tablename__ = 'genre'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(255))
+
+
+class GenreSchema(Schema):
+    id = fields.Int()
+    name = fields.Str()
+
+
+class User(db.Model):
+    __tablename__ = 'user'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String)
+    password = db.Column(db.String)
+    role = db.Column(db.String)
+
+    def get_hash(self):
+        return hashlib.md5(self.password.encode('utf-8')).hexdigest()
+
+
+class UserSchema(Schema):
+    id = fields.Int()
+    username = fields.Str()
+    password = fields.Str()
+    role = fields.Str()
+
+
+class Auth:
+    def __init__(self, user_service: User):
+        self.user_service = user_service
+
+    def generate_tokens(self, username, password, is_refresh=False):
+        user = self.user_service.get_by_username(username)
+        if user is None:
+            raise Exception()
+        if not is_refresh:
+            if not self.user_service.compare_passwords(user.password, password):
+                raise Exception()
+
+        data = {
+            "username": user.username,
+            "role": user.role
+        }
+        min30 = datetime.datetime.utcnow() + datetime.timedelta(minutes=30)
+        data["exp"] = calendar.timegm(min30.timetuple())
+        access_token = jwt.encode(data, JWT_SECRET, algorithm=JWT_ALGORITHM)
+
+        days130 = datetime.datetime.utcnow() + datetime.timedelta(days=130)
+        data["exp"] = calendar.timegm(days130.timetuple())
+        refresh_token = jwt.encode(data, JWT_SECRET, algorithm=JWT_ALGORITHM)
+        return {"access_token": access_token, "refresh_token": refresh_token}
+
+    def approve_refresh_token(self, refresh_token):
+        data = jwt.decode(jwt=refresh_token, key=JWT_SECRET, algorithms=[JWT_ALGORITHM])
+        username = data.get("username")
+        user = self.user_service.get_by_username(username)
+        if user is None:
+            raise Exception()
+        return self.generate_tokens(username, user.password, is_refresh=True)
+
+
---
+++ HW_19/hard/lesson19_project_hard_source-main/views/users.py
@@ -0,0 +1,45 @@
+from flask import request
+
+from flask_restx import Resource, Namespace
+
+from dao.model.user import UserSchema
+from decorators import auth_required, admin_required
+from implemented import user_service
+
+user_ns = Namespace('users')
+
+
+@user_ns.route('/')
+class UsersView(Resource):
+    @auth_required
+    def get(self):
+        rs = user_service.get_all()
+        res = UserSchema(many=True).dump(rs)
+        return res, 200
+
+    def post(self):
+        request_json = request.json
+        user = user_service.create(request_json)
+        return "", 201, {"location": f"/users/{user.id}"}
+
+
+@user_ns.route('/<int:rid>')
+class UserView(Resource):
+    @auth_required
+    def get(self, rid):
+        r = user_service.get_one(rid)
+        sm_d = UserSchema().dump(r)
+        return sm_d, 200
+
+    @admin_required
+    def put(self, rid):
+        req_json = request.json
+        if "id" not in req_json:
+            req_json["id"] = rid
+        user_service.update(req_json)
+        return "", 204
+
+    @admin_required
+    def delete(self, rid):
+        user_service.delete(rid)
+        return "", 204
